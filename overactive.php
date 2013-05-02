9c40ff99c32c6ab78ebc5c629c4d4eb34196d2c5
Alastair Wankel <alastair.wankel@gmail.com>
2013-05-02 14:48:05 -0700

    dkdk

--- overactive.php
+++ overactive.php
@@ -6,9 +6,10 @@
 
 <form >
 What is your name?<input type="text" name="name"><br>
-What is your quest?<input type="text" name="quest"<br>
-What is your favorite color?<input type="text" name="favColor">
-<input type="submit" value="submit to me">
+What is your quest?<input type="text" name="quest"><br>
+What is your favorite color?<input type="text" name="favColor"><br>
+<input type="submit" value="submit to me"><br>
+
 
 
 </form>
