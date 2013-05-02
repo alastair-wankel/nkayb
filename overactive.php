b1c6a786015e6f019b780c7bc88b8c2f5fcc6a36
Alastair Wankel <alastair.wankel@gmail.com>
2013-05-02 14:43:12 -0700

    ramdom php stuff

--- overactive.php
+++ overactive.php
@@ -4,9 +4,14 @@
 
 <h1>My first PHP page</h1>
 
-<?php
-echo "Hello World!";
-?>
+<form >
+What is your name?<input type="text" name="name"><br>
+What is your quest?<input type="text" name="quest"<br>
+What is your favorite color?<input type="text" name="favColor">
+<input type="submit" value="submit to me">
+
+
+</form>
 
 </body>
 </html>
