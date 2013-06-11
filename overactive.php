df0b3159615e71b8d063f60cae37ec763991a387
Alastair Wankel <alastair.wankel@gmail.com>
2013-06-11 15:42:58 -0700

    dj

--- overactive.php
+++ overactive.php
@@ -25,12 +25,12 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="brand" href="#">Project name</a>
+          <a class="brand" href="#">Nkayb.com</a>
           <div class="nav-collapse collapse">
             <ul class="nav">
-              <li class="active"><a href="#">Home</a></li>
-              <li><a href="#about">About</a></li>
-              <li><a href="#contact">Contact</a></li>
+              <li class="active"><a href="http://www.linkedin.com/in/alastairwankel">Who am I</a></li>
+              <li><a href="#about">My Projects</a></li>
+              <li><a href="#contact">My Hobbies</a></li>
               <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
                 <ul class="dropdown-menu">
@@ -44,11 +44,7 @@
                 </ul>
               </li>
             </ul>
-            <form class="navbar-form pull-right">
-              <input class="span2" type="text" placeholder="Email">
-              <input class="span2" type="password" placeholder="Password">
-              <button type="submit" class="btn">Sign in</button>
-            </form>
+            
           </div><!--/.nav-collapse -->
         </div>
       </div>
