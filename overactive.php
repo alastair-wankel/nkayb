7e3ec8321a055038118ce0ded02fc5a3631be3eb
Alastair Wankel <alastair.wankel@gmail.com>
2013-06-11 11:45:49 -0700

    djdj

--- overactive.php
+++ overactive.php
@@ -2,17 +2,6 @@
 <html>
 <body>
 
-<h1>Test forms</h1>
-
-<form >
-What is your name?<input type="text" name="name"><br>
-What is your quest?<input type="text" name="quest"><br>
-What is your favorite color?<input type="text" name="favColor"><br>
-<input type="submit" value="submit to me"><br>
-
-
-
-</form>
 
 </body>
 </html>
