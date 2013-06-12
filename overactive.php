63ccb3f31ccf063c144bd53f756b6ba7bcf0ae7e
Alastair Wankel <alastair.wankel@gmail.com>
2013-06-12 16:15:16 -0700

    djg

--- overactive.php
+++ overactive.php
@@ -72,57 +72,84 @@
       </div>
 
       <!-- Example row of columns -->
-      <div class="container">
-    <div class="carousel slide" id="myCarousel">
-        <div class="carousel-inner">
-          <div class="item active">
-                <ul class="thumbnails span12">
-                    <li>
-                        <div class="caption">
-                            <h5>Caption</h5>
-                        </div>
-                        <div class="thumbnail">
-                            <img src="http://placehold.it/260x180" alt="">
-                        </div>
-                    </li>
-                    <li>
-                        <div class="caption">
-                            <h5>Caption</h5>
-                        </div>
-                        <div class="thumbnail">
-                            <img src="http://placehold.it/260x180" alt="">
-                        </div>
-                    </li>
-                    <li>
-                        <div class="caption">
-                            <h5>Caption</h5>
-                        </div>
-                        <div class="thumbnail">
-                            <img src="http://placehold.it/260x180" alt="">
-                        </div>
-                    </li>
-                    <li>
-                        <div class="caption">
-                            <h5>Caption</h5>
-                        </div>
-                        <div class="thumbnail">
-                            <img src="http://placehold.it/260x180" alt="">
-                        </div>
-                    </li>
-                </ul>
-          </div>         
-          <div class="item">
-               ...
-          </div>
-          <div class="item">
-               ...
-          </div>
-
-        </div>
-        <a data-slide="prev" href="#myCarousel" class="left carousel-control">‹</a>
-        <a data-slide="next" href="#myCarousel" class="right carousel-control">›</a>
-    </div>
-</diV>
+      <div class="carousel slide span8" id="myCarousel">
+<div class="carousel-inner">
+  <div class="item active">
+        <ul class="thumbnails">
+            <li class="span2">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/260x180" alt="">
+                </div>
+            </li>
+            <li class="span2">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/260x180" alt="">
+                </div>
+            </li>
+            <li class="span2">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/260x180" alt="">
+                </div>
+            </li>
+            <li class="span2">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/260x180" alt="">
+                </div>
+            </li>
+        </ul>
+  </div>
+  <div class="item">
+        <ul class="thumbnails">
+            <li class="span2">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/260x180" alt="">
+                </div>
+            </li>
+            <li class="span2">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/260x180" alt="">
+                </div>
+            </li>
+            <li class="span2">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/260x180" alt="">
+                </div>
+            </li>
+            <li class="span2">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/260x180" alt="">
+                </div>
+            </li>
+        </ul>
+  </div>
+  <div class="item">
+        <ul class="thumbnails">
+            <li class="span2">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/260x180" alt="">
+                </div>
+            </li>
+            <li class="span2">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/260x180" alt="">
+                </div>
+            </li>
+            <li class="span2">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/260x180" alt="">
+                </div>
+            </li>
+            <li class="span2">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/260x180" alt="">
+                </div>
+            </li>
+        </ul>
+  </div>
+</div>
+<a data-slide="prev" href="#myCarousel" class="left carousel-control">‹</a>
+<a data-slide="next" href="#myCarousel" class="right carousel-control">›</a>
+</div>
 
       <hr>
 
