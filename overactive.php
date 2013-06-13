4d73ee1194bb7ba754128c2f923e806194b2ba9c
Alastair Wankel <alastair.wankel@gmail.com>
2013-06-13 14:18:02 -0700

    gjt

--- overactive.php
+++ overactive.php
@@ -13,7 +13,7 @@
     <style type="text/css">
     
     #div1, #div2, #div3, #div4
-    {float: right; width:80px; height:105px; margin:-10px; padding:0px;border:1px solid #aaaaaa;}
+    {float: right; width:80px; height:105px; margin:0px; margin-left: 1px; margin-right: 1px;  padding:0px;border:1px solid #aaaaaa;}
       body {
         padding-top: 60px;
         padding-bottom: 40px;
