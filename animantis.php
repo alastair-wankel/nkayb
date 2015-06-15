b15ccbe0c5dbb2ee3aede8c6457b7e6a06e4bd37
Alastair Wankel <alastair.wankel@gmail.com>
2015-06-15 14:42:40 -0700

    idh

--- animantis.php
+++ animantis.php
@@ -35,7 +35,7 @@
           <div class="nav-collapse collapse">
             <ul class="nav">
               <li><a href="#about">About</a></li>
-              <li><a href="#contact">Contact</a></li>
+              <li><a href="#contact">Contact</a><span class="glyphicons-user"></span></li>
               
             </ul>
             <form class="navbar-form pull-right">
