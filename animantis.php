ff04a99ea4ff53ef4130191764b01a9c21542811
Alastair Wankel <alastair.wankel@gmail.com>
2015-08-21 15:41:13 -0700

    gradient testing

--- animantis.php
+++ animantis.php
@@ -28,7 +28,13 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
     <style type="text/css">
-          body { background: -webkit-linear-gradient(white, white, #86c67c); !important }
+    #grad1 {
+    
+    background: -webkit-linear-gradient(white, white, #86c67c); /* For Safari 5.1 to 6.0 */
+    background: -o-linear-gradient(white, white, #86c67c); /* For Opera 11.1 to 12.0 */
+    background: -moz-linear-gradient(white, white, #86c67c); /* For Firefox 3.6 to 15 */
+    background: linear-gradient(white, white, #86c67c); !important/* Standard syntax (must be last) */
+          }     
     </style>
   </head>
 
