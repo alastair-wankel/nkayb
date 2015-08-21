3dc07dfcb3ebd99fb57ac4899d66b112585bc52e
Alastair Wankel <alastair.wankel@gmail.com>
2015-08-21 15:48:43 -0700

    jd

--- animantis.php
+++ animantis.php
@@ -13,7 +13,7 @@
     <title>Animantis LLC</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+   
 
     <!-- Custom styles for this template -->
     <link href="starter-template.css" rel="stylesheet">
@@ -33,7 +33,7 @@
     background: -webkit-linear-gradient(white, #86c67c); /* For Safari 5.1 to 6.0 */
     background: -o-linear-gradient(white, #86c67c); /* For Opera 11.1 to 12.0 */
     background: -moz-linear-gradient(white, #86c67c); /* For Firefox 3.6 to 15 */
-    background: linear-gradient(white, #86c67c); !important/* Standard syntax (must be last) */
+    background: linear-gradient(white, #86c67c); /* Standard syntax (must be last) */
           }     
     </style>
   </head>
