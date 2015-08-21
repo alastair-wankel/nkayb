c15c8bbebe6cc448875ac7b72238070496630e7b
Alastair Wankel <alastair.wankel@gmail.com>
2015-08-21 15:42:21 -0700

    th

--- animantis.php
+++ animantis.php
@@ -30,10 +30,10 @@
     <style type="text/css">
     #grad1 {
     
-    background: -webkit-linear-gradient(white, white, #86c67c); /* For Safari 5.1 to 6.0 */
-    background: -o-linear-gradient(white, white, #86c67c); /* For Opera 11.1 to 12.0 */
-    background: -moz-linear-gradient(white, white, #86c67c); /* For Firefox 3.6 to 15 */
-    background: linear-gradient(white, white, #86c67c); !important/* Standard syntax (must be last) */
+    background: -webkit-linear-gradient(white, #86c67c); /* For Safari 5.1 to 6.0 */
+    background: -o-linear-gradient(white, #86c67c); /* For Opera 11.1 to 12.0 */
+    background: -moz-linear-gradient(white, #86c67c); /* For Firefox 3.6 to 15 */
+    background: linear-gradient(white, #86c67c); !important/* Standard syntax (must be last) */
           }     
     </style>
   </head>
