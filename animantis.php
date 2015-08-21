d4fd047b5cca17076f21cc4e526a40288fd6e9aa
Alastair Wankel <alastair.wankel@gmail.com>
2015-08-21 16:04:17 -0700

    grad

--- animantis.php
+++ animantis.php
@@ -27,9 +27,8 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
     <style type="text/css">
-    #grad1 {
-    
-    background: -webkit-linear-gradient(white, #86c67c); /* For Safari 5.1 to 6.0 */
+    #grad !important {
+            background: -webkit-linear-gradient(white, #86c67c); /* For Safari 5.1 to 6.0 */
     background: -o-linear-gradient(white, #86c67c); /* For Opera 11.1 to 12.0 */
     background: -moz-linear-gradient(white, #86c67c); /* For Firefox 3.6 to 15 */
     background: linear-gradient(white, #86c67c); /* Standard syntax (must be last) */
