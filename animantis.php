5036efdec29b9ec093c4356a27951e40d83a09b8
Alastair Wankel <alastair.wankel@gmail.com>
2015-08-28 10:21:46 -0700

    thing

--- animantis.php
+++ animantis.php
@@ -28,11 +28,9 @@
     <![endif]-->
     <style type="text/css">
     
-            background: -webkit-linear-gradient(white, #86c67c); /* For Safari 5.1 to 6.0 */
-            background: -o-linear-gradient(white, #86c67c); /* For Opera 11.1 to 12.0 */
-            background: -moz-linear-gradient(white, #86c67c); /* For Firefox 3.6 to 15 */
-            background: linear-gradient(white, #86c67c); /* Standard syntax (must be last) */
             
+            background: linear-gradient(white, #86c67c); /* Standard syntax (must be last) */
+          
     </style>
   </head>
 
