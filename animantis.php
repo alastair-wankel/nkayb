66384b5418bff4a7db455b0bd62c77a805970465
Alastair Wankel <alastair.wankel@gmail.com>
2015-08-28 10:27:25 -0700

    Gradient testing continues

--- animantis.php
+++ animantis.php
@@ -26,12 +26,7 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <style type="text/css">
-    
-            
-            background: linear-gradient(white, #86c67c); /* Standard syntax (must be last) */
-          
-    </style>
+   
   </head>
 
   <body>
