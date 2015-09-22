ba8baa99d55f0464bf8fb1fe4c9a58452a9d2fe2
Alastair Wankel <alastair.wankel@gmail.com>
2015-09-22 15:38:18 -0700

    think

--- animantis.php
+++ animantis.php
@@ -30,8 +30,8 @@
 
   </head>
 
-  <body class="grad">
-  
+  <body>
+      <div class="container grad">
       <nav class="navbar navbar-fixed-top">
       <div class="container grad" >
         <div class="navbar-header">
@@ -54,7 +54,7 @@
         </div><!--/.nav-collapse -->
       </div>
     </nav>
-  
+  </div>
   
     
 
